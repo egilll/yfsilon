383313c8063522ec8413afebf3e65fc27ca73385
Egill <egillsigurdur@gmail.com>
2020-02-24 23:04:34 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+/*
+
+  Öll i yfir í y
+
+*/
+(function() {
+  function skipta(input) {
+    return input
+      .replace(/(i)/g, 'y')
+      .replace(/(I)/g, 'Y')
+      .replace(/(í)/g, 'ý')
+      .replace(/(Ý)/g, 'Ý')
+  }
+  function findText(node) {
+    if (node.nodeType == 3) {
+      node.data = skipta(node.data)
+    }
+    if (node.nodeType == 1 && node.nodeName != "SCRIPT") {
+      for (var i = 0; i < node.childNodes.length; i++) {
+        findText(node.childNodes[i])
+      }
+    }
+  }
+  findText(document.body)
+})()
+/*
+
+  Öll y yfir í í
+
+*/
+(function() {
+  function skipta(input) {
+    return input
+      .replace(/(y)/g, 'i')
+      .replace(/(Y)/g, 'I')
+      .replace(/(ý)/g, 'í')
+      .replace(/(Ý)/g, 'Ý')
+  }
+  function findText(node) {
+    if (node.nodeType == 3) {
+      node.data = skipta(node.data)
+    }
+    if (node.nodeType == 1 && node.nodeName != "SCRIPT") {
+      for (var i = 0; i < node.childNodes.length; i++) {
+        findText(node.childNodes[i])
+      }
+    }
+  }
+  findText(document.body)
+})()
